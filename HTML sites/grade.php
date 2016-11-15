5fc0ae2815e36444fcb21fcade1467e7479e6fdd
Squidward Tentacles - Fermini <luca.fermini18@alumni.loyola.ca>
2016-11-15 11:38:49 -0500

    Fix
    fixed my program, testing out quiz, trying to make it work

---
+++ HTML sites/grade.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+<title>Premier League Goals</title>
+<link rel="stylesheet" href="main.css"><!-- Links this HTML to CSS -->
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
